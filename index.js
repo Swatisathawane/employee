7732521ce7d59edf9093824b9be9cbb07b8430c7
Swatisathawane <106958388+Swatisathawane@users.noreply.github.com>
2023-01-14 11:00:01 +0530

    push

---
+++ index.js
@@ -0,0 +1,113 @@
+// console.log("hello world");
+// cons name="Swati";
+// name="swati Sathawane";
+// console.log(name);
+
+
+// var name="Swati";
+// Var age=23
+// var is18above=true;
+// console.log(name,age,is18above);
+// console.log("my name is"+name+"and my age is"+age+"i am above 18"+is18above);
+
+
+// var name="Swati";
+// var age=23;
+// var is18above=true;
+// console.log(name,age,is18above);
+// console.log("my name is"+name+"and my age is"+age+"i am above 18"+is18above);
+// console.log('my name is ${name} and my age is ${age} i am above 18 {is18above}');
+
+
+// var num=20;
+// var output=num + num;
+// console.log(output);
+
+
+//**OPERATION  */
+
+
+// const num1=10;
+// const num2=20;
+// const num3="10";
+// console.log(num1<=10 || num2<=20);
+// console.log(num1>=10 && num2>=20);
+// console.log(num1>20 || num2<10);
+// console.log(num1===num2);
+// console.log(num1==num3);
+// console.log(num1===num3);
+
+
+//**TERNARY OPRATION */
+
+// const num1=10;
+// const num2="10";
+// const output=num1==num2 ? +num2+num1: "nothing";
+// console.log(output);
+
+
+
+
+//*******************************
+// Loops---> Iterator of Work 
+
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+// 4. for of loop
+// 5. for in loop
+// // s. foreach loop
+
+// for(let i=0;i<1000;i++)
+// {
+//     console.log(i);
+
+// }
+// console.log("done");
+
+
+//*** Infinite loop */
+// for(let i;true;)
+// {
+//     console.log(i);
+
+// }
+// console.log("done");
+
+// let i
+// while(i<1000)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// let i;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<1000)
+
+
+
+const arr=["Grapes","Strawberr*y","Orange","Apple","Banana","Tomato",10,20,30,40,true,false,[1,2,3,4,5]];
+arr.push("kivi"); // insert data at last index
+arr.unshift("Mango");  // insert data at first index
+console.log(arr);
+const lastele =arr.pop();
+console.log(lastele);
+
+const firstele =arr.shift();
+console.log(firstele);
+console.log(arr);
+
+
+
+
+// for(let i=0;i<6;i++){
+//     console.log(arr[i]);
+// }
+
+
+
+    
